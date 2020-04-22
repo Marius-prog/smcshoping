59b74ef12d92523d2baea015df10dd95d17f9bc6
Marius Sabaliauskas <msabaliauskas@gmail.com>
2020-04-22 15:20:50 +0200

    security dependency added..

--- src/main/java/com/shoping/Common.java
+++ src/main/java/com/shoping/Common.java
@@ -1,6 +1,7 @@
 package com.shoping;
 
 
+import com.shoping.models.Cart;
 import com.shoping.models.CategoryRepository;
 import com.shoping.models.PageRepository;
 import com.shoping.models.data.Category;
@@ -10,9 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
 
 @ControllerAdvice
+@SuppressWarnings("unchecked")
 public class Common {
 
     @Autowired
@@ -22,13 +26,34 @@ public class Common {
     private CategoryRepository categoryRepo;
 
     @ModelAttribute
-    public void sharedData(Model model){
+    public void sharedData(Model model, HttpSession session) {
 
         List<Page> pages = pageRepo.findAllByOrderBySortingAsc();
 
         List<Category> categories = categoryRepo.findAll();
 
+        boolean cartActive = false;
+
+        if (session.getAttribute("cart") != null) {
+
+            HashMap<Integer, Cart> cart = (HashMap<Integer, Cart>) session.getAttribute("cart");
+
+            int size = 0;
+            double total = 0;
+
+            for (Cart value : cart.values()) {
+                size += value.getQuantity();
+                total += value.getQuantity() * Double.parseDouble(value.getPrice());
+            }
+
+            model.addAttribute("csize", size);
+            model.addAttribute("ctotal", total);
+
+            cartActive = true;
+        }
+
         model.addAttribute("cpages", pages);
         model.addAttribute("ccategories", categories);
+        model.addAttribute("cartActive", cartActive);
     }
 }
